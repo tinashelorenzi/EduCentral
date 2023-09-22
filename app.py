513d2a0c6d0a6801f0bbb8e01e79da3a630fe7dc
Tinashe <tinashelorenzi@protonmail.com>
2023-09-22 20:18:58 +0200

    Update

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from handlers import login_handler,cipherguard, database_handler
 
 from cs50 import SQL
 app = Flask(__name__)
+app.debug = True
 enckey = "95fb144ffdb4a1899ce187a015ec02a21a227018"
 app.secret_key = 'Shall I compare thee to a summers day'
+adminallowed = ['admin','reception']
 
 @app.route("/")
 def index():
@@ -50,5 +52,104 @@ def logout():
     session['authenticated'] = False
     return redirect('/')
             
-        
+@app.route('/register_learner') 
+def register_learner():
+    userstatus = database_handler.getuserstatus(session['userid'])
+    if userstatus in adminallowed:
+        return render_template('register.html')
+    else:
+        return 'Not allowed to view this region'
+
+@app.route('/parents_database')
+def parents_database():
+    userstatus = database_handler.getuserstatus(session['userid'])
+    if not session.get('authenticated'):
+        return redirect('/')  
+    if userstatus in adminallowed:
+        parents = database_handler.get_parents()
+        id = session.get('userid')
+        username = database_handler.if_fetch_name(id)
+        return render_template('database_parents.html', parents=parents,username=username)
+    else:
+        return 'Not allowed to view this region'
     
+@app.route("/submit_studentreg",methods=["GET", "POST"])
+def student_review():
+    userstatus = database_handler.getuserstatus(session['userid'])
+    if userstatus in adminallowed:
+        if request.method == "GET":
+            return redirect('/register_learner')
+        name = request.form["name"]
+        surname = request.form["surname"]
+        dob = request.form["date_of_birth"]
+        address = request.form["address"]
+        idnr = request.form["idnr"]
+        grade = request.form["grade"]
+        parentidnr = request.form["parentidnr"]
+        Parentname = request.form["Parentname"]
+        Parentsurname = request.form["Parentsurname"]
+        monthlyfee = request.form["monthlyfee"]
+        allergies = request.form["allergies"]
+        Ename = request.form["Ename"]
+        Etel = request.form["Etel"]
+        econditions = request.form["emergency-conditions"]
+        dietres = request.form["dietres"]
+        langs = request.form["langs"]
+        pschool = request.form["pschool"]
+        reasprev = request.form["reasprev"]
+        snotes = request.form["snotes"]
+        sports = request.form["sports"]
+        parentemail = request.form["parentemail"]
+        parenttel = request.form["parenttel"]
+        medicine = request.form["med"]
+        image_file = request.files['image']
+        gender = request.form["gender"]
+        new_file_name = idnr
+        new_file_path = os.path.join('uploads/', new_file_name)
+        image_file.save(new_file_path)
+        
+        #Create parent first
+        database_handler.submit_parent(parentidnr,Parentname,Parentsurname,parenttel,parentemail,address)
+        
+        #Push to review
+        database_handler.submit_review(name,surname,dob,address,parentidnr,grade,monthlyfee,allergies,
+                  sports,idnr,Ename,Etel,
+                  econditions,medicine,dietres,langs,
+                  pschool,reasprev,snotes,gender)
+        return redirect('/register_learner')
+    else:
+        return redirect('/')
+    
+@app.route('/review_students')
+def rev_students():
+    userstatus = database_handler.getuserstatus(session['userid'])
+    if not session.get('authenticated'):
+        return redirect('/') 
+    if userstatus in adminallowed:
+        registrations = database_handler.get_student_reviews()
+        return render_template('review_registrations.html', applications=registrations)
+@app.route('/decline',methods=["POST"])
+def decline_student():
+        if not session.get('authenticated'):
+            return redirect('/') 
+        if request.method == 'POST':
+            applicant_id = request.form['student_id']
+
+        # Connect to the SQLite database
+            conn = sqlite3.connect('database/student_data.db')
+            cursor = conn.cursor()
+
+        # Delete the password by its ID
+            query = "DELETE FROM student_review WHERE id = ?"
+            cursor.execute(query, (applicant_id,))
+
+        # Commit the changes and close the connection
+            conn.commit()
+            conn.close()
+
+        # Redirect back to the dashboard
+        return redirect('/review_students')
+    
+
+if __name__ == "__main__":
+    app.run(debug=True, port=4444)
--- handlers/database_handler.py
+++ handlers/database_handler.py
@@ -42,6 +42,29 @@ def if_fetch_name(user_id):
     else:
         return 0
     
+def getuserstatus(user_id):
+    # Connect to the database
+    conn = sqlite3.connect('database/users.db')
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # Execute a SQL query to select the name of the user with the given ID
+    cur.execute('SELECT authority_level FROM users WHERE id = ?', (user_id,))
+
+    # Fetch the results of the query
+    result = cur.fetchone()
+
+    # Close the cursor and connection
+    cur.close()
+    conn.close()
+
+    # If a user was found, return their name. Otherwise, return 0.
+    if result is not None:
+        return result[0]
+    else:
+        return 0
+    
 def learner_count():
     conn = sqlite3.connect('database/student_data.db')
     cur = conn.cursor()
@@ -59,3 +82,56 @@ def parent_count():
     cur.close()
     conn.close()
     return result[0]
+
+def get_parents():
+    conn = sqlite3.connect('database/student_data.db') 
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM parents")
+    columns = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+
+    data = []
+    for row in rows:
+        data.append(dict(zip(columns, row)))
+
+    conn.close()
+    return data
+
+def submit_parent(parentidnr,Parentname,Parentsurname,ParentTel,email,address):
+    db_file = "database/student_data.db"
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    query = f"INSERT INTO parents (name,surname,identity_number,tel,address,email) VALUES (?,?,?,?,?,?);"
+    cursor.execute(query, (Parentname,Parentsurname,parentidnr,ParentTel,address,email))
+    conn.commit()
+    conn.close()
+    print("Parent created successfully!")
+def submit_review(name,surname,date_of_birth,address,parent_id,grade,monthly_fee,allergies,
+                  sports,identity_number,emergency_contact_name,emergency_contact_number,
+                  medical_conditions,medication,dietary_restrictions,languages_spoken,
+                  previous_school,reason_for_leaving_previous_school,notes,gender):
+    db_file = "database/student_data.db"
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    query = f"INSERT INTO student_review (name,surname,date_of_birth,address,parent_id,grade,monthly_fee,allergies,sports,identity_number,emergency_contact_name,emergency_contact_number,medical_conditions,medication,dietary_restrictions,languages_spoken,previous_school,reason_for_leaving_previous_school,notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+    cursor.execute(query, (name,surname,date_of_birth,address,parent_id,grade,monthly_fee,allergies,
+                  sports,identity_number,emergency_contact_name,emergency_contact_number,
+                  medical_conditions,medication,dietary_restrictions,languages_spoken,
+                  previous_school,reason_for_leaving_previous_school,notes,gender))
+    conn.commit()
+    
+def get_student_reviews():
+    conn = sqlite3.connect('database/student_data.db') 
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM student_review")
+    columns = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+
+    data = []
+    for row in rows:
+        data.append(dict(zip(columns, row)))
+
+    conn.close()
+    return data
